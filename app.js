60827782af4ebe9c8e85f4c9e64b56211aa39900
anand-yolo <anand.singh@yolobus.in>
2026-06-30 19:04:28 +0530

    comment code

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
 
 const express = require("express")
+require('dotenv').config();
 const connectDB = require("./config/db");
 const PORT = process.env.PORT
+console.log("PORT.............",PORT)
 
 const app = express();
 
--- config/db.js
+++ config/db.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log("MongoDB Connected");
-  } catch (error) {
-    console.log("MongoDB connection error:", error);
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
