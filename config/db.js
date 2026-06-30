aa57bb6ae34670236a38309788b896388379d782
Ubuntu <ubuntu@ip-172-31-35-45.ap-south-1.compute.internal>
2026-06-30 12:53:19 +0000

    sjhdskj

--- config/db.js
+++ config/db.js
@@ -5,13 +5,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
-<<<<<<< HEAD
-  } catch (error) {
-    console.log({error});
-=======
   } catch ("eeeeeeeeeeeeeeeeeeeee",error) {
     console.log(error);
->>>>>>> 13172cb (s)
     process.exit(1);
   }
 };
