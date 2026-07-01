0d105ad7b6757085297b5be2eeb2e0f5a4ef6235
anand-yolo <anand.singh@yolobus.in>
2026-07-01 12:21:50 +0530

    snnkdn

--- config/db.js
+++ config/db.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    let URI
+    if(process.env.ENOVEMRT == "test"){
+      URI = process.env.MONGO_URI 
+    }else{
+        URI = process.env.PROD_URI
+    }
+    await mongoose.connect(URI);
+    console.log("MongoDB Connected",URI);
   } catch (err) {
     console.error("errrrrrrrrrrrr",err);
     // process.exit(1);
